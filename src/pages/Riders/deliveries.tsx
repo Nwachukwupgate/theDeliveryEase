6377f8883c6792a7145c747f1fc33b9a97ef30fa
treyp99 <nwachukwupgate99@gmail.com>
2024-10-25 20:30:20 +0100

    new push

--- src/pages/Riders/deliveries.tsx
+++ src/pages/Riders/deliveries.tsx
@@ -1,85 +1,85 @@
 import DeliveringCard from "./components/DeliveringCard";
-import { useRiderDashboardQuery, useRiderDeliveriesQuery } from "@/api/apiSlice";
+import { useRiderDeliveriesQuery } from "@/api/apiSlice";
 import { CircularProgress } from "@mui/material";
 import { Link } from 'react-router-dom';
 
 
-const deliveries = [
-    {
-      id: "AZ34KLO",
-      delivery: "Same Day Delivery",
-      address: "Malu road Idumota, Abuja State",
-      status: "On the way",
-      date: "14 May 2024, 3:04 PM",
-      product: "Macbook",
-      weight: "2 kg",
-      trackingId: "AZ34KLO",
-      stops: [
-          {
-            label: "Delivery Booked",
-            location: "2, Malu road Idumota, Abuja",
-            status: "Done",
-            time: "14 May 2024, 3:04 PM",
-          },
-          {
-            label: "Arrived at pick up point",
-            location: "2, Malu road Idumota, Abuja",
-            status: "Done",
-            time: "14 May 2024, 3:04 PM",
-          },
-          {
-            label: "Route to delivery",
-            location: "2, Malu road Idumota, Abuja",
-            status: "Ongoing",
-            time: "Pending",
-          },
-          {
-            label: "Arrived at delivery",
-            location: "Pending",
-            status: "Pending",
-            time: "Pending",
-          },
-          {
-            label: "Delivery accepted",
-            location: "Pending",
-            status: "Pending",
-            time: "Pending",
-          },
-      ],
-      mapUrl: "https://via.placeholder.com/600x400.png?text=Map", // Placeholder image
-    },
-    {
-      id: "BZ23POI",
-      delivery: "Next Day Delivery",
-      address: "Malu road Idumota, Abuja State",
-      status: "Pending",
-      date: "14 May 2024, 3:04 PM",
-      product: "Laptop",
-      weight: "1.8 kg",
-      trackingId: "BZ23POI",
-      stops: [
-          {
-            label: "Delivery Booked",
-            location: "3, Malu road Idumota, Abuja",
-            status: "Done",
-            time: "15 May 2024, 3:04 PM",
-          },
-          {
-            label: "Arrived at pick up point",
-            location: "3, Malu road Idumota, Abuja",
-            status: "Pending",
-            time: "Pending",
-          },
-          {
-            label: "Route to delivery",
-            location: "Pending",
-            status: "Pending",
-            time: "Pending",
-          },
-      ],
-      mapUrl: "https://via.placeholder.com/600x400.png?text=Map",
-    },
-  ];
+// const deliveries = [
+//     {
+//       id: "AZ34KLO",
+//       delivery: "Same Day Delivery",
+//       address: "Malu road Idumota, Abuja State",
+//       status: "On the way",
+//       date: "14 May 2024, 3:04 PM",
+//       product: "Macbook",
+//       weight: "2 kg",
+//       trackingId: "AZ34KLO",
+//       stops: [
+//           {
+//             label: "Delivery Booked",
+//             location: "2, Malu road Idumota, Abuja",
+//             status: "Done",
+//             time: "14 May 2024, 3:04 PM",
+//           },
+//           {
+//             label: "Arrived at pick up point",
+//             location: "2, Malu road Idumota, Abuja",
+//             status: "Done",
+//             time: "14 May 2024, 3:04 PM",
+//           },
+//           {
+//             label: "Route to delivery",
+//             location: "2, Malu road Idumota, Abuja",
+//             status: "Ongoing",
+//             time: "Pending",
+//           },
+//           {
+//             label: "Arrived at delivery",
+//             location: "Pending",
+//             status: "Pending",
+//             time: "Pending",
+//           },
+//           {
+//             label: "Delivery accepted",
+//             location: "Pending",
+//             status: "Pending",
+//             time: "Pending",
+//           },
+//       ],
+//       mapUrl: "https://via.placeholder.com/600x400.png?text=Map", // Placeholder image
+//     },
+//     {
+//       id: "BZ23POI",
+//       delivery: "Next Day Delivery",
+//       address: "Malu road Idumota, Abuja State",
+//       status: "Pending",
+//       date: "14 May 2024, 3:04 PM",
+//       product: "Laptop",
+//       weight: "1.8 kg",
+//       trackingId: "BZ23POI",
+//       stops: [
+//           {
+//             label: "Delivery Booked",
+//             location: "3, Malu road Idumota, Abuja",
+//             status: "Done",
+//             time: "15 May 2024, 3:04 PM",
+//           },
+//           {
+//             label: "Arrived at pick up point",
+//             location: "3, Malu road Idumota, Abuja",
+//             status: "Pending",
+//             time: "Pending",
+//           },
+//           {
+//             label: "Route to delivery",
+//             location: "Pending",
+//             status: "Pending",
+//             time: "Pending",
+//           },
+//       ],
+//       mapUrl: "https://via.placeholder.com/600x400.png?text=Map",
+//     },
+//   ];
 
 const DeliveriesPage = () => {
 
