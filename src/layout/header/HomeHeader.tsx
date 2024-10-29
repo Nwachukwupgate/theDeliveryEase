a75be659c2d108a547c490fb67c7286bc29128db
treyp99 <nwachukwupgate99@gmail.com>
2024-10-29 11:50:24 +0100

    new push

--- src/layout/header/HomeHeader.tsx
+++ src/layout/header/HomeHeader.tsx
@@ -38,7 +38,7 @@ const HomeHeader = () => {
 
         {/* Mobile Menu open: "block", Menu closed: "hidden" */}
         <div className={`bg-primaryHoverColorDark opacity-80 md:hidden absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:items-center ${isOpen ? 'block opacity-100 translate-x-0' : 'hidden opacity-0 -translate-x-full'}`}>
-          <div className="flex flex-col items-center h-[calc(87vh-30px)] md:h-auto md:flex-row md:mx-6 pt-12 space-y-2">
+          <div className="flex flex-col items-center h-[calc(87vh-50px)] md:h-auto md:flex-row md:mx-6 pt-12 space-y-2">
             <Link className="my-2 text-white transition-colors duration-300 transform border-b-2 border-white px-32 py-4 hover:text-white md:mx-4 md:my-0" to="/">ABOUT</Link>
             <Link className="my-2 text-white transition-colors duration-300 transform border-b-2 border-white px-32 py-4 hover:text-white md:mx-4 md:my-0" to="/shop">SERVICE</Link>
             <Link className="my-2 text-white transition-colors duration-300 transform border-b-2 border-white px-32 py-4 hover:text-white md:mx-4 md:my-0" to="/contact">TRACK</Link>
