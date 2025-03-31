14aff30f4d0eb23e8d117c10449022a7ffe6746f
oghene-kevwe <idisepraise@gmail.com>
2025-03-31 19:27:27 +0100

    fixed welcome loader
    Signed-off-by: oghene-kevwe <idisepraise@gmail.com>

--- src/common/loaders/WelcomeLoader.tsx
+++ src/common/loaders/WelcomeLoader.tsx
@@ -1,13 +1,14 @@
+import loadingIcon from "../../assets/image/deliveryEaseLoading.gif";
+
 const WelcomeLoader: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-purple-800">
-      <div className="animate-spin mb-4">
-        <img src="https://s3-alpha-sig.figma.com/img/b463/0fb2/21a28241bd491f73eef9a5c02cc1fadb?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MdvDcRaMrhQrJf08Gx88Kq-IJALYEXzoQtF9NunOfgrRJBRJ9Bw37K8dlq9Gcx3RFFVacA-PC6fihXm5PdGpzSgj3X9EQ0jdag39czpMJ7IxGW4n-nZ~x6YRntPtPgqlkgS~ZkaHislkp81MqCAt9BkPxitB-GDY3ScyWCiCpxHeDcpnQenB0Kd5lTVAYuegZcs8K2r5V2BLB3BgkQLG84481q1MPce-QSrwNGYR0jkW3vF-AIXauYTeCcLsi-fUzz1QzsfcaHlMJYVGVbZWVfdcJB3IKAhgV98bc8TsHyC9-GGGJn3YjpTSv2HF5CGmMwQbA6lCeVqKFxBSYPr7FA__" alt="Loading..." className="w-32 h-32" />
+    <div className="flex h-screen flex-col items-center justify-center bg-purple-800">
+      <div className="mb-4">
+        <img src={loadingIcon} alt="Loading..." className="h-32 w-32" />
       </div>
-      <p className="text-white text-xl">Ease In</p>
+      <p className="text-xl text-white">Ease In</p>
     </div>
   );
 };
-  
+
 export default WelcomeLoader;
-  
