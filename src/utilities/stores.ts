e9af76da61ead1f4c82d0cd5d21a8e55689e0357
Idise Praise <idisepraise@gmail.com>
2025-04-08 14:03:02 +0100

    added user check method
    Signed-off-by: Idise Praise <idisepraise@gmail.com>

--- src/utilities/stores.ts
+++ src/utilities/stores.ts
@@ -1,21 +1,40 @@
-import { proxy } from 'valtio';
+import { proxy } from "valtio";
 
 // Define the initial state
 const userStore = proxy({
   token: null as string | null,
-  user: null as { name?: string; email: string,  id?: number;  } | null,
-  userType: null as 'admin' | 'user' | 'rider' | null,
+  user: null as { name?: string; email: string; id?: number } | null,
+  userType: null as "admin" | "user" | "rider" | null,
+
+  // Getter method to check if the user is a rider
+  get isRider(): boolean {
+    return this.userType === "rider";
+  },
+
+  // Getter method to check if the user is an admin
+  get isAdmin(): boolean {
+    return this.userType === "admin";
+  },
+
+  // Getter method to check if the user is a regular user
+  get isRegularUser(): boolean {
+    return this.userType === "user";
+  },
 
   // Method to log in the user and save to localStorage
-  loginUser(token: string, user: {id?: number; name?: string; email: string }, userType: 'admin' | 'user' | 'rider') {
+  loginUser(
+    token: string,
+    user: { id?: number; name?: string; email: string },
+    userType: "admin" | "user" | "rider",
+  ) {
     this.token = token;
     this.user = user;
     this.userType = userType;
 
     // Save the data in localStorage
-    localStorage.setItem('DELogisticsToken', token);
-    localStorage.setItem('user', JSON.stringify(user));
-    localStorage.setItem('userType', userType);
+    localStorage.setItem("DELogisticsToken", token);
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("userType", userType);
   },
 
   // Method to log out the user and clear the state and localStorage
@@ -24,22 +43,22 @@ const userStore = proxy({
     this.user = null;
     this.userType = null;
 
-    localStorage.removeItem('DELogisticsToken');
-    localStorage.removeItem('user');
-    localStorage.removeItem('userType');
+    localStorage.removeItem("DELogisticsToken");
+    localStorage.removeItem("user");
+    localStorage.removeItem("userType");
   },
 });
 
 // Load any saved data from localStorage on app initialization
 const loadUserDataFromLocalStorage = () => {
-  const token = localStorage.getItem('DELogisticsToken');
-  const user = localStorage.getItem('user');
-  const userType = localStorage.getItem('userType');
+  const token = localStorage.getItem("DELogisticsToken");
+  const user = localStorage.getItem("user");
+  const userType = localStorage.getItem("userType");
 
   if (token && user && userType) {
     userStore.token = token;
     userStore.user = JSON.parse(user);
-    userStore.userType = userType as 'admin' | 'user';
+    userStore.userType = userType as "admin" | "user";
   }
 };
 
