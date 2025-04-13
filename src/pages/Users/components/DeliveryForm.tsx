bb9ba1db9f33380791c05486bcf0c336ff135269
Idise Praise <idisepraise@gmail.com>
2025-04-13 22:22:52 +0100

    Revert "init mapbox"
    This reverts commit 023051eec62080f31929f4bcd0285beb7cd3c6a6.

--- src/pages/Users/components/DeliveryForm.tsx
+++ src/pages/Users/components/DeliveryForm.tsx
@@ -4,6 +4,8 @@ import { useForm } from "react-hook-form";
 import { Button } from "@mui/material";
 import NameIcon from "@/common/icons/NameIcon";
 import PhoneIcon from "@/common/icons/PhoneIcon";
+// import AddressIcon from "@/common/icons/AddressIcon";
+// import UnknownIcon from "@/common/icons/UnknownIcon";
 import ProductNameIcon from "@/common/icons/ProductNameIcon";
 import ProductDescriptionIcon from "@/common/icons/ProductDescriptionIcon";
 import WeightIcon from "@/common/icons/WeightIcon";
@@ -12,10 +14,9 @@ import WalletIcon from "@/common/icons/WalletIcon";
 import { DeliveryReq } from "@/types/types";
 import { DeliveryType } from "@/utilities/constants";
 import { useState } from "react";
-import MapboxAutocomplete from "@/components/tracker/MapBoxAutoComplete"; // Import the new component
+import Autocomplete from 'react-google-autocomplete';
 
-// Replace with your Mapbox access token
-const mapboxToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+const googlemapskey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
 const DeliveryForm: React.FC<{ onSubmit: (data: DeliveryReq) => void }> = ({
   onSubmit,
@@ -27,6 +28,7 @@ const DeliveryForm: React.FC<{ onSubmit: (data: DeliveryReq) => void }> = ({
     setValue
   } = useForm<DeliveryReq>();
 
+
   const [pickupCoordinates, setPickupCoordinates] = useState<{
     latitude: number | null;
     longitude: number | null;
@@ -37,32 +39,26 @@ const DeliveryForm: React.FC<{ onSubmit: (data: DeliveryReq) => void }> = ({
     longitude: number | null;
   }>({ latitude: null, longitude: null });
 
-  const handlePickupSelect = (place: any) => {
-    if (place) {
-      setValue('pickup_address', place.formatted_address || '');
-      if (place.geometry?.location) {
-        setPickupCoordinates({
-          latitude: place.geometry.location.lat(),
-          longitude: place.geometry.location.lng(),
-        });
-      }
+  const handlePickupSelect = (place: google.maps.places.PlaceResult | null) => {
+    setValue('pickup_address', place?.formatted_address || '');
+    if (place?.geometry?.location) {
+      setPickupCoordinates({
+        latitude: place.geometry.location.lat(),
+        longitude: place.geometry.location.lng(),
+      });
     } else {
-      setValue('pickup_address', '');
       setPickupCoordinates({ latitude: null, longitude: null });
     }
   };
 
-  const handleDeliverySelect = (place: any) => {
-    if (place) {
-      setValue('delivery_address', place.formatted_address || '');
-      if (place.geometry?.location) {
-        setDeliveryCoordinates({
-          latitude: place.geometry.location.lat(),
-          longitude: place.geometry.location.lng(),
-        });
-      }
+  const handleDeliverySelect = (place: google.maps.places.PlaceResult | null) => {
+    setValue('delivery_address', place?.formatted_address || '');
+    if (place?.geometry?.location) {
+      setDeliveryCoordinates({
+        latitude: place.geometry.location.lat(),
+        longitude: place.geometry.location.lng(),
+      });
     } else {
-      setValue('delivery_address', '');
       setDeliveryCoordinates({ latitude: null, longitude: null });
     }
   };
@@ -77,6 +73,7 @@ const DeliveryForm: React.FC<{ onSubmit: (data: DeliveryReq) => void }> = ({
     };
     onSubmit(finalData);
   };
+
 
   return (
     <form onSubmit={handleSubmit(onSubmitHandler)} className="bg-white p-4 lg:p-8">
@@ -120,48 +117,70 @@ const DeliveryForm: React.FC<{ onSubmit: (data: DeliveryReq) => void }> = ({
       <h3 className="mt-4 text-lg font-bold">Delivery Details</h3>
 
       <div className="mt-6 grid grid-cols-1 gap-8 lg:grid-cols-2">
+        {/* <IconInput
+          placeholder="Pickup Address"
+          name="pickup_address"
+          register={register}
+          error={errors.pickup_address}
+          icon={<AddressIcon />}
+        /> */}
+
         <div>
           <label htmlFor="pickup_address" className="block text-sm font-medium text-gray-700">
             Pickup Address
           </label>
-          <MapboxAutocomplete
-            apiKey={mapboxToken}
+          <Autocomplete
+            apiKey={googlemapskey}
             placeholder="Pickup Address"
-            onPlaceSelected={handlePickupSelect}
-            onInputChange={(value) => {
-              setValue('pickup_address', value);
-              setPickupCoordinates({ latitude: null, longitude: null });
+            onPlaceSelected={(place) => handlePickupSelect(place)}
+            options={{
+              componentRestrictions: { country: 'ng' }, // Optional: Restrict to Nigeria
+              types: ['address'], // Optional: Restrict to address results
             }}
-            className={`w-full rounded-3xl border px-4 py-2.5 shadow-sm focus:outline-none focus:ring-2 focus:ring-primary ${
-              errors.pickup_address
-                ? "border-error bg-errorBg placeholder-errorLight"
-                : "border-gray-300 bg-primaryActiveColorLight hover:border-gray-400"
-            }`}
-            country="ng" // Nigeria
+            className={`w-full rounded-3xl border px-4 py-2.5 shadow-sm focus:outline-none focus:ring-2 focus:ring-primary ${errors.pickup_address
+              ? "border-error bg-errorBg placeholder-errorLight"
+              : "border-gray-300 bg-primaryActiveColorLight hover:border-gray-400"
+              } `}
+            onInputChange={(event: { target: { value: string; }; }) => {
+              setValue('pickup_address', event.target.value);
+              setPickupCoordinates({ latitude: null, longitude: null }); // Clear coordinates on input change
+            }}
+            defaultValue={errors.pickup_address?.message as string} // Optional: Handle initial value or errors
           />
           {errors.pickup_address && (
             <p className="mt-1 text-red-500 text-sm">{errors.pickup_address.message}</p>
           )}
         </div>
 
+        {/* <IconInput
+          placeholder="Delivery Address"
+          name="delivery_address"
+          register={register}
+          error={errors.delivery_address}
+          icon={<UnknownIcon />}
+        /> */}
+
         <div>
           <label htmlFor="delivery_address" className="block text-sm font-medium text-gray-700">
             Delivery Address
           </label>
-          <MapboxAutocomplete
-            apiKey={mapboxToken}
+          <Autocomplete
+            apiKey="YOUR_Maps_API_KEY" // Replace with your actual API key
             placeholder="Delivery Address"
-            onPlaceSelected={handleDeliverySelect}
-            onInputChange={(value) => {
-              setValue('delivery_address', value);
-              setDeliveryCoordinates({ latitude: null, longitude: null });
+            onPlaceSelected={(place) => handleDeliverySelect(place)}
+            options={{
+              componentRestrictions: { country: 'ng' }, // Optional: Restrict to Nigeria
+              types: ['address'], // Optional: Restrict to address results
             }}
-            className={`w-full rounded-3xl border px-4 py-2.5 shadow-sm focus:outline-none focus:ring-2 focus:ring-primary ${
-              errors.delivery_address
-                ? "border-error bg-errorBg placeholder-errorLight"
-                : "border-gray-300 bg-primaryActiveColorLight hover:border-gray-400"
-            }`}
-            country="ng" // Nigeria
+            className={`w-full rounded-3xl border px-4 py-2.5 shadow-sm focus:outline-none focus:ring-2 focus:ring-primary ${errors.delivery_address
+              ? "border-error bg-errorBg placeholder-errorLight"
+              : "border-gray-300 bg-primaryActiveColorLight hover:border-gray-400"
+              }`}
+            onInputChange={(event: { target: { value: string; }; }) => {
+              setValue('delivery_address', event.target.value);
+              setDeliveryCoordinates({ latitude: null, longitude: null }); // Clear coordinates on input change
+            }}
+            defaultValue={errors.delivery_address?.message as string} // Optional: Handle initial value or errors
           />
           {errors.delivery_address && (
             <p className="mt-1 text-red-500 text-sm">{errors.delivery_address.message}</p>
@@ -204,6 +223,15 @@ const DeliveryForm: React.FC<{ onSubmit: (data: DeliveryReq) => void }> = ({
       <h3 className="mt-4 text-lg font-bold">Delivery & Payment Type</h3>
 
       <div className="my-6 grid grid-cols-1 gap-8 lg:grid-cols-2">
+        {/* <SelectDropdown
+          label="Payment Option"
+          name="payment_option"
+          register={register}
+          error={errors.payment_option}
+          options={[{ value: "Bank Transfer", label: "Bank Transfer" }]}
+          icon={<WalletIcon />}
+        /> */}
+
         <SelectDropdown
           label="Delivery Type"
           name="delivery_type"
